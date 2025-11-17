82211d3859aa314ecd71ad15adab4e6f44c3d396
Rahul735124 <146526571+Rahul735124@users.noreply.github.com>
2025-11-18 00:39:21 +0530

    Change BASE_URL to production endpoint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
 
-export const BASE_URL="http://localhost:8002"
+export const BASE_URL="https://chatting-app-backend-8hkk.onrender.com"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
