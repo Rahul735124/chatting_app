f8b59077a7f5a6ca6256ea5d74f44b120953478d
Rahul735124 <146526571+Rahul735124@users.noreply.github.com>
2025-11-19 15:49:29 +0530

    Refactor CORS settings and clean up socket.js
    Refactor socket.io CORS configuration and improve code formatting.

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,38 +1,37 @@
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 
 const app = express();
-
 const server = http.createServer(app);
+
 const io = new Server(server, {
-    cors:{
-        origin:['https://chatting-app-frontend-lrli.onrender.com'],
-        methods:['GET', 'POST'],
-    },
+  cors: {
+    origin: "https://chatting-app-frontend-lrli.onrender.com",
+    methods: ["GET", "POST"],
+    credentials: true
+  },
 });
 
 export const getReceiverSocketId = (receiverId) => {
-    return userSocketMap[receiverId];
-}
+  return userSocketMap[receiverId];
+};
 
-const userSocketMap = {}; // {userId->socketId}
+const userSocketMap = {}; // { userId : socketId }
 
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
 
-io.on('connection', (socket)=>{
-    const userId = socket.handshake.query.userId
-    if(userId !== undefined){
-        userSocketMap[userId] = socket.id;
-    } 
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
 
-    io.emit('getOnlineUsers',Object.keys(userSocketMap));
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on('disconnect', ()=>{
-        delete userSocketMap[userId];
-        io.emit('getOnlineUsers',Object.keys(userSocketMap));
-    })
+  socket.on("disconnect", () => {
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
-})
-
-export {app, io, server};
-
+export { app, io, server };
